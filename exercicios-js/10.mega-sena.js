924c2147b0396f6d397ec9eaf5400c33ae00ba9d
Lucas Costa <englucascbmelo@gmail.com>
2023-07-07 16:03:05 -0300

    aplicando método para não repetir números

--- exercicios-js/10.mega-sena.js
+++ exercicios-js/10.mega-sena.js
@@ -5,31 +5,38 @@
 
 
 let myNumbers = [5, 7, 21, 6, 45, 75, 32];
-let loterryNumbers = [];
+let lotteryNumbers = [];
 
 
 for (let index = 0; index < myNumbers.length; index = index + 1){
-    loterryNumbers.push(Math.ceil(Math.random() * 60));
+
+    let randomNumber = Math.ceil(Math.random() * 60);
+
+    if(!lotteryNumbers.includes(randomNumber)){
+        lotteryNumbers.push(randomNumber);
+    } else {
+        index = index - 1;
+    }
 };
 
-console.log(loterryNumbers);
+console.log(lotteryNumbers);
 
 //Comparação do sorteio com o jogo do usuário
 
 let count = 0;
 let acertos = [];
 
-for (let indexLottery = 0; indexLottery < loterryNumbers.length; indexLottery = indexLottery + 1){
+for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery = indexLottery + 1){
 
     for(let indexGame = 0; indexGame < myNumbers.length; indexGame = indexGame + 1){
-        if(loterryNumbers[indexLottery] === myNumbers[indexGame]){
+        if(lotteryNumbers[indexLottery] === myNumbers[indexGame]){
             count = count + 1;
-            acertos.push(loterryNumbers[indexLottery]);
+            acertos.push(lotteryNumbers[indexLottery]);
         }
     }
 }
 //Quantidade de acertos
-console.log(`Os número da loteria foram: ${loterryNumbers}`);
+console.log(`Os números da loteria foram: ${lotteryNumbers}`);
 console.log(`Os números que o usuário escolheu foram: ${myNumbers}`);
 console.log(count);
 console.log(acertos);
