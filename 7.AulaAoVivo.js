3e7e6d604c59cd56b364ca9f284622e1026d0279
Lucas Costa <englucascbmelo@gmail.com>
2023-07-08 18:29:11 -0300

    praticando aula ao vivo

--- 7.AulaAoVivo.js
+++ 7.AulaAoVivo.js
@@ -1,1 +1,22 @@
 // Exercicio de colocar os números em ordem crescente dentro da Array
+
+function ordemCrescente(numbers){
+
+    for(index = 0; index < numbers.length; index += 1){
+
+        if(numbers[index] > numbers[index + 1]){
+
+            numbers[index +1] = numbers[index];
+
+        } else {
+
+
+
+        }
+
+    }
+
+    return `a ordem crescente dos elementos é ${numbers}`;
+};
+
+console.log(ordemCrescente([5,2,6,9,3]));
