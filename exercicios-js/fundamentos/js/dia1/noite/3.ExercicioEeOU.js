7bb7aec81cf8fb6c7403d25e0f7f893ab9320076
Lucas Costa <englucascbmelo@gmail.com>
2023-07-10 23:25:17 -0300

    corrigindo pastas

---
+++ exercicios-js/fundamentos/js/dia1/noite/3.ExercicioEeOU.js
@@ -0,0 +1,14 @@
+//EXERCICIO 3
+
+let num5 = 22;
+let num6 = 31;
+let num7 = 54; 
+
+if(num5>num6 && num5>num7){
+    console.log(`o número ${num5} é o maior de todos\n`);
+} else if (num5<num6 && num6>num7){
+    console.log(`o número ${num6} é o maior de todos\n`)
+} else{
+    console.log(`o último é o maior de todos\n`)
+}
+
---
+++ exercicios-js/fundamentos/js/dia1/noite/5.ExercicioSwitchCase.js
@@ -0,0 +1,46 @@
+//EXERCICO 5
+
+let carrinho = "uvas";
+
+switch(carrinho){
+    case "laranjas":
+        console.log("As laranjas custam R$ 2,00 o quilo\n");
+        break;
+    case "bananas":
+        console.log("As bananas custam R$ 3,00 o quilo\n");
+        break;
+    case "uvas":
+        console.log("As uvas custam R$ 1,00 o quilo\n");
+        break;        
+}
+
+var Animal = 'Girafa';
+switch (Animal) {
+    case 'Vaca':
+    case 'Girafa':
+    case 'Cachorro':
+    case 'Porco':
+        console.log('Esse animal irá para Arca de Noé\n');
+        break;
+    case 'Dinossauro':
+        break;
+    default:
+        console.log('Esse animal não vai.\n');
+}
+
+
+//Ainda dentro do exercicio 5, mas caso Xadrez
+
+let brick = "cavalo";
+
+switch(brick){
+    case "peão":
+        console.log("move 1 casa na ortogonal\n");
+        break;
+    case "cavalo":
+        console.log("anda em L\n"); 
+        break;
+    case "bispo":
+        console.log("anda nas diagonais\n");  
+}
+ 
